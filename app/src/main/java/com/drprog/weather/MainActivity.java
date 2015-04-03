b86b56833a6e77546e5382b0abd683435d801770
drstranges <drstranges@gmail.com>
2015-04-03 10:54:56 +0300

    added MainActivity, layouts for MainActivity, ToolBar, applied styles

---
+++ app/src/main/java/com/drprog/weather/MainActivity.java
@@ -0,0 +1,22 @@
+package com.drprog.weather;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+    }
+}
